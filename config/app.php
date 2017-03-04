7e4a90823c06b936ac990a735d33bc9a357ca8fc
Antoine Masline camerini <antoinemaslinecamerini@mbp-de-antoine.home>
2017-03-04 10:35:41 +0100

    ajout du package "laravelcollective/html": "5.4.*". création du système de connection : php artisan make:auth

--- config/app.php
+++ config/app.php
@@ -176,7 +176,12 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        /*
+         * Package non inclus par défault
+         * Inclu par Antoine le 04-03-2017
+         */
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -225,6 +230,13 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        /*
+         * Package non inclus par défault
+         * Inclu par Antoine le 04-03-2017
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
